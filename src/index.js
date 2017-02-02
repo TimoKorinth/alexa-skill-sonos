768c45ad8af03707530840558e9649e9b946aaaa
Timo <timo.korinth@maximago.de>
2017-02-02 16:52:48 +0100

    Added initial code

---
+++ src/index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/*
+ * App ID for the skill
+ */
+var APP_ID = undefined;
+
+/*
+ * Environment Configuration
+ */
+var config = {};
+config.IOT_BROKER_ENDPOINT      = "identifier.iot.us-east-1.amazonaws.com".toLowerCase();
+config.IOT_BROKER_REGION        = "us-east-1";
+
+//Loading AWS SDK libraries
+var AWS = require('aws-sdk');
+AWS.config.region = config.IOT_BROKER_REGION;
+//Initializing client for IoT
+var iotData = new AWS.IotData({endpoint: config.IOT_BROKER_ENDPOINT});
+var topic = "/sonos";
+
+var Alexa = require("alexa-sdk");
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'PushMessage': function (payload, tell) {
+        var params = {
+            topic: topic,
+            payload: payload,
+            qos:0
+        };
+        iotData.publish(params, (err, data) => {
+            if (!err){
+                this.emit(':tell', tell);
+            }   
+        });
+    },
+    /*'LaunchRequest': function () {
+        this.emit('StartIntent');
+    },*/
+    'StartIntent': function () {
+        var sonos = this.event.request.intent.slots.Sonos.value;
+        this.emit('PushMessage', 'start' + sonsos, 'ok');
+    },
+    'StopIntent': function () {
+        var sonos = this.event.request.intent.slots.Sonos.value;
+        this.emit('PushMessage', 'stop' + sonos, 'ok');
+    },
+    /*'ChangeChannelIntent': function () {
+        var channel = this.event.request.intent.slots.Channel.value;
+        this.emit('PushMessage', channel, 'ok');
+    },
+    'VolUpIntent': function () {
+        this.emit('PushMessage', 'volup', 'ok');
+    },
+    'VolDownIntent': function () {
+        this.emit('PushMessage', 'voldown', 'ok');
+    },*/
+    "Unhandled": function () {
+        this.emit(':tell', 'keine ahnung');
+    }
+};
